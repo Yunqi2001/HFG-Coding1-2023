5b138ae6265571f448570ba9a1bbc6e9112321ed
Yunqi Li <56251623+Yunqi2001@users.noreply.github.com>
2023-06-13 15:08:36 +0200

    0613 update
    -changed table to div
    -basic functions realised but still needs improvement

--- my_final_project_Yunqi/memory.js
+++ my_final_project_Yunqi/memory.js
@@ -1,10 +1,10 @@
 let pics = [
     '01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '010.jpg', '011.jpg', '012.jpg', '013.jpg', '014.jpg', '015.jpg', '016.jpg', '017.jpg', '018.jpeg', '019.jpg', '020.jpg','01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '010.jpg', '011.jpg', '012.jpg', '013.jpg', '014.jpg', '015.jpg', '016.jpg', '017.jpg', '018.jpeg', '019.jpg', '020.jpg']
+  
 
 let flippedCards = []
-let firstCard = null;
-let secondCard = null;
 let currentIndex = 0;
+
 
 function shuffle(array){
     for(let i = array.length - 1; i>0; i--){
@@ -13,81 +13,109 @@ function shuffle(array){
         array[i] = array[j];
         array[j] = temp;
     }
+    return array;
 }
-shuffle(pics);
+
+pics = shuffle(pics);
+
+
+function createCard(pic){
+    let card = document.createElement("div");
+    card.className = "card";
+    card.setAttribute("data-symbol", pic);
+    card.setAttribute("data-flipped", "false");
+
+    let display = document.createElement("img");
+    display.src = "back.jpg";
+    card.appendChild(display);
+
+    return card;
+}
+
 
 function createTable(){
-    let table = document.getElementById("gameTable");
+    let table = document.getElementById("gameContainer");
     for(let i=0;i<5;i++){
         let row = document.createElement("tr");
         for (let j=0; j<8;j++){
             let cell = document.createElement("td");
-            let card = document.createElement("img");
-
-            card.src = "back.jpg";
-            card.setAttribute("data-flipped", "false");
-            card.setAttribute("data-symbol", pics[currentIndex]);
-            currentIndex ++;
-
+            let front = pics[currentIndex];
+            let card = createCard(front);
+            card.addEventListener("click", function(){
+                flipCard(card);
+            });
+            
             cell.appendChild(card);
-            cell.onclick = function(){
-                flipCard(this);
-            };
             row.appendChild(cell);
-        }
+            currentIndex ++;
+            };
         table.appendChild(row);
     }
 }
 
 createTable();
 
-function flipCard(cell){
-    let card = cell.getElementsByTagName("img")[0];
-    if (card.getAttribute("data-flipped") === "true"){
+
+function flipCard(card){
+    if(card.classList.contains("flipped")){
         return;
     }
 
-    card.src = card.getAttribute("data-symbol");
-    card.setAttribute("data-flipped", "true");
+    card.classList.add("flipped");
     flippedCards.push(card);
 
-    if (flippedCards.length === 2){
-        setTimeout(lookCards, 10000);
+    card.firstChild.src = card.getAttribute("data-symbol");
+
+    if(flippedCards.length === 2){
+        lookCards();
     }
-    if(flippedCards.length >2){
+    if(flippedCards.length > 2){
         resetCards();
     }
 
 }
+
 
 function lookCards(){
-    firstCard = flippedCards[0];
-    secondCard = flippedCards[1];
+    let firstCard = flippedCards[0];
+    let secondCard = flippedCards[1];
 
-    if(firstCard.src === secondCard.src){
-        firstCard.parentNode.remove();
-        secondCard.parentNode.remove();
+    if(firstCard.getAttribute("data-symbol") === secondCard.getAttribute("data-symbol")) {
+        firstCard.setAttribute("data-flipped", "true");
+        secondCard.setAttribute("data-flipped", "true");
+
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+
+        setTimeout(removeMatched,1000);
+
         flippedCards = [];
-    }else{
-        resetCards();
+        checkGameOver();
     }
 }
 
-function removeCards(card1,card2){
-    let parent1 = card1.parentNode;
-    let parent2 = card2.parentNode;
-    let placeholder = document.createElement("td");
-    parent1.replaceChild(placeholder, card1);
-
-}
 
 function resetCards() {
     for (let i = 0; i < flippedCards.length; i++) {
         let card = flippedCards[i];
-        card.src = "back.jpg";
-        card.setAttribute("data-flipped", "false");
+        card.classList.remove("flipped");
+        card.firstChild.src = "back.jpg";
     }
     flippedCards = [];
 }
 
-//try to use <div> instead of table (older), look at some current examples
+
+function removeMatched(){
+    let matchedCards = document.querySelectorAll(".card.matched");
+    matchedCards.forEach(function (card) {
+        card.parentNode.removeChild(card);
+    });
+}
+
+
+function checkGameOver() {
+    let remainingCards = document.querySelectorAll(".card:not(.matched)");
+    if (remainingCards.length === 0) {
+      alert("Congratulations! You have completed the game!");
+    }
+  }
